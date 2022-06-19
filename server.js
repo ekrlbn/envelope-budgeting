8da9123327aad224a6e9f47d3aff36c6d33f68fc
ekrlbn <105484430+ekrlbn@users.noreply.github.com>
2022-06-19 09:48:57 +0300

    other dependencies installed and configured

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
 	res.send("HELLO, WORLD");
 });
